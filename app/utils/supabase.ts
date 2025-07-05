915a4324d99e381183918117289c49cec2900a29
Trey Green <w.treygreen@gmail.com>
2025-07-05 15:47:14 -0400

    supabase client

--- app/utils/supabase.ts
+++ app/utils/supabase.ts
@@ -1,12 +1,19 @@
 import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // Initialize Supabase client
-const supabaseUrl = process.env.NEXT_SUPABASE_URL!;
-const supabaseServiceKey = process.env.NEXT_SUPABASE_SERVICE_ROLE_KEY!;
+const supabaseUrl = process.env.NEXT_SUPABASE_URL;
+const supabaseServiceKey = process.env.NEXT_SUPABASE_SERVICE_ROLE_KEY;
 
 // Create a single supabase client for server-side usage
-export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey, {
-  auth: {
-    persistSession: false,
-  }
-});
+export const supabaseAdmin = createClient(
+    supabaseUrl || '', 
+    supabaseServiceKey || '', 
+    {
+      auth: {
+        persistSession: false,
+      }
+    }
+  );
